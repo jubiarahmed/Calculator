3b1ee26e530fc433850ae85469912cab3d868722
jubiarahmed <mdjubiarahmedrabbi@gmail.com>
2024-03-14 04:24:02 +0600

    Calculator

--- Program.cs
+++ Program.cs
@@ -1,3 +1,51 @@
 ﻿Console.WriteLine("Welcome To The Console Project (Calculator)");
+
 Console.WriteLine("Input the first number");
 var firstAsText = Console.ReadLine();
+var number1 = int.Parse(firstAsText);
+
+Console.WriteLine("Input the second number");
+var secondAsText = Console.ReadLine();
+var number2 = int.Parse(secondAsText);
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubstract numbers");
+Console.WriteLine("[M]ultiply numbers"); 
+
+var choice = Console.ReadLine();
+
+if(EqualCaseInsensitive(choice, "A"))
+{
+    var sum = number1 + number2;
+    PrintFinalEquation(number1, number2, sum, "+");
+}
+
+else if (EqualCaseInsensitive(choice, "S"))
+{
+    var difference = number1 - number2;
+    PrintFinalEquation(number1, number2, difference, "-");
+}
+
+if (EqualCaseInsensitive(choice, "M"))
+{
+    var multiplied = number1 * number2;
+    PrintFinalEquation(number1, number2, multiplied, "*");
+}
+else
+{
+    Console.WriteLine("Invalid choice!");
+}
+
+Console.WriteLine("Press any key close"); 
+Console.ReadKey();
+
+void PrintFinalEquation(int number1, int number2, int result, string @operator)
+{
+    Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + result);
+}
+
+bool EqualCaseInsensitive(string left, string right)
+{ 
+return left.ToUpper().Equals(right.ToUpper());
+}
--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae4f3b9aff5665dc408f24692ab5d8e4109c1e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8093bd5fef9717cd198a9773a03afcd490c9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
